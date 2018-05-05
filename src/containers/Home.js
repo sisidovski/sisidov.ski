9fc186d9e2d84170f72b6ff4a62f3c7cd43f5127
sisidovski <arerreee@gmail.com>
2018-05-06 06:45:40 +0900

    Add text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,6 +7,7 @@ import BlogItem from '../components/BlogItem'
 
 export default withRouteData(({ about, posts }) => (
   <div>
+    Recent Posts:
     <ul className="blog-items">
       {posts
         .slice(0, 5)
