4a26f7660707322d966d2a1ec38a6649e1e83755
sisidovski <arerreee@gmail.com>
2018-09-29 22:09:15 +0100

    Add CV

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link, NavLink } from 'react-static'
 
+const publicCVURL = 'https://docs.google.com/document/d/1bI4G3AYuosCsoz959AqeF_8PZ2QIKB2TANqMx5ZIvgs/edit?usp=sharing'
+
 export default () => (
   <header className="header">
     <Link className="header__title" to="/">Shunya Shishido</Link>
     <nav>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/blog">Blog</NavLink>
+      <Link to={publicCVURL} target="_blank">CV</Link>
     </nav>
   </header>
 )
