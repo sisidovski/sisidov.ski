4e76004689b494ed9098b56d4f37bcfdf14f8c4f
sisidovski <arerreee@gmail.com>
2018-12-26 18:42:32 +0900

    Add sxg

--- scripts/make-firebase-config.js
+++ scripts/make-firebase-config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
 
 
 const signedExchangeConfig = {
-  source: '**/*.sxg.dummy',
+  source: '**/*.sxg',
   headers: [
     {
       key: 'Content-Type',
